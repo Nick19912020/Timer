ad68f0645db7f993e1239aa44e5bc4e9fff7983e
Kukarkin Nikolay <n.kukarkin1991@gmail.com>
2022-02-27 13:52:58 +0300

    20 задание

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import timer from './modules/timer'
-//import twoJS from './modules/two'
+import menu from './modules/menu'
+import modal from './modules/modal'
 
-timer("28 february 2022")
-//twoJS()
+timer('28 february 2022');
+menu();
+modal();
---
+++ src/modules/modal.js
@@ -0,0 +1,38 @@
+
+const modal = () => {
+    const buttons = document.querySelectorAll('.popup-btn');
+    const modal = document.querySelector('.popup');
+    const closeBtn = modal.querySelector('.popup-close');
+    const modalContent = document.querySelector('.popup-content')
+    let animation;
+    let count = -70;
+    let widthScreen = window.screen.width;
+
+    function appear() {
+        count++
+        animation = requestAnimationFrame(appear);
+        if (count > 20) {
+            cancelAnimationFrame(animation)
+        } else {
+            modalContent.style.top = count * 6 + 'px'
+        }
+    }
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            if (widthScreen < 768) {
+                modal.style.display = 'block';
+            } else {
+                modal.style.display = 'block';
+                appear();
+            }
+        })
+    })
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+        count = -70;
+    })
+}
+
+export default modal 
